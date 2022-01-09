535e6de0f50abab24aba2ea7e915d41d39e6ba91
olebergens <infiziert.1337@gmail.com>
2022-01-09 14:32:18 +0100

    yeee

--- hausaufgabenserie/serie04/Aufgabe1.c
+++ hausaufgabenserie/serie04/Aufgabe1.c
@@ -7,45 +7,26 @@ typedef struct {
     DATA d;
 }listElement;
 
-void showList(listElement* start) {
-    if (start != NULL){
-        listElement  *p = start;
-        printf("%s", p->d.chr);
-        while (p->next != NULL) {
-            p = p->next;
-            printf("%s", p->d.chr);
-        }
-    } else  {
-        printf("Die List ist leer!\n");
-    }
-}
-
-int appendElement(listElement** start, const DATA* dInp) {
-    listElement *newElement = (listElement*) malloc(sizeof(listElement));
-    if (newElement != NULL) {
-        newElement->d.chr = dInp->chr;
-        newElement->next = NULL;
-        if (*start == NULL) {
-            *start = newElement;
-        } else {
-            listElement *n = *start;
-            while (n->next != NULL) n = n->next;
-            n->next = newElement;
-        }
-        return 0;
-    }
-    return -1;
-}
-
 int start(void) {
-    listElement * start = NULL;
-    DATA myD;
-    myD.chr = "Hallo\n";
-    appendElement(&start, &myD);
-    myD.chr = "Welt\n";
-    appendElement(&start, &myD);
-    myD.chr = "!";
-    appendElement(&start, &myD);
-    showList(start);
+    FILE *file;
+    char line[128];
+    listElement *current, *head;
+
+    head = current = NULL;
+    file = fopen("/Users/ole/Desktop/Universität Rostock/Imperative Programmierung/ok0579/hausaufgabenserie/serie04/moby-dick.txt", "r");
+
+    while (fgets(line, sizeof(line), file)) {
+        listElement *node = malloc(sizeof(listElement));
+        node->d.chr = strdup(line);
+        node->next = NULL;
+        if (head == NULL)
+            current = head = node;
+        else
+            current = current->next = node;
+    }
+    fclose(file);
+    for (current = head; current; current=current->next) {
+        printf("%s", current->d.chr);
+    }
     return 0;
 }
