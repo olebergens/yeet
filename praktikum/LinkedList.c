740b04c17e20a1a109b7587d894e52319253499c
olebergens <infiziert.1337@gmail.com>
2022-01-12 12:37:48 +0100

    jesus sei mit mir

--- praktikum/LinkedList.c
+++ praktikum/LinkedList.c
@@ -12,4 +12,25 @@ struct _list *linsert(int v, struct _list *list) {
     return list;
 }
 
-void printIter()
+void printIter(struct _list *list) {
+    while (list != NULL) {
+        printf("value: %d\n", list->v);
+        list = list->next;
+    }
+}
+
+void printRec(struct _list *list) {
+    if (list != NULL) {
+        printf("value: %d\n", list->v);
+        printRec(list->next);
+    }
+}
+
+int l = 0;
+int lengthIter(struct _list *list) {
+    if (list == NULL) return l;
+    else {
+        l++;
+        lengthIter(list->next);
+    }
+}
